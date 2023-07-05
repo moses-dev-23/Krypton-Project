c28030994413210ca690637414faf94fa6193622
Moses <moses.tech.dev@gmail.com>
2023-07-05 19:14:59 -0400

    Created buttons and grid to test. Working on event listeners

---
+++ js/app.js
@@ -0,0 +1,34 @@
+console.log("Hello")
+
+//LOOPING METHOD
+
+// Create array to hold hidden word letters
+let hiddenWord = ["gravity"];
+
+// Create an empty array to store clicked letters. 
+let selectedLetters = [];
+
+
+
+//Add event listener
+
+//Get letter elements
+let libraryLetters = document.getElementsByClassName("letter");
+
+// Create function for matching arrays
+
+
+
+
+// function for click event. 
+// If correct, disable button, adjust visibility in hidden word
+//If incorrect, disable button, error alert  
+
+//library of buttons with letter displayed.
+
+
+//Create Grid in CSS to hold library of 12 letters. By default all letters will be transparent. When the array matches, then change visibilty. 
+
+//Disable the button after clicked
+
+//
