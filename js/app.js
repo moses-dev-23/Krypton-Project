e02d3df391eefe9ff449c42d39c3430722f164a3
Moses <moses.tech.dev@gmail.com>
2023-07-13 18:34:55 -0400

    Added rocket pieces and logic. Currently troubleshooting win and loss messages.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 
 
-//LOOPING METHOD
-
 // Array to hold hidden word letters
 let hiddenWord = ["g","r","a","v","i","t","y"];        
 
@@ -31,6 +29,40 @@ const tButton = document.querySelector('#button-t');
 const kButton = document.querySelector('#button-k');
 const yButton = document.querySelector('#button-y');
 
+//DOM: Rocket pieces in staging area
+const rocket1 = document.querySelector('.engine');
+const rocket2 = document.querySelector('.mainBody');
+const rocket3 = document.querySelector('.left-fin');
+const rocket4 = document.querySelector('.right-fin');
+const rocket5 = document.querySelector('.nose');
+const rocket6 = document.querySelector('#fire');
+const rocket7 = document.querySelector('#flame');
+
+//DOM for loss message
+const lMessage = document.querySelector('#message-l')
+
+//DOM for reset button
+const resetButton = document.querySelector('#resetButton');
+
+// Process for displaying rocket pieces in order
+let rocketPieces = [rocket1, rocket2, rocket3, rocket4, rocket5, rocket6, rocket7];
+
+let incorrectCount = 0;
+
+function displayRocketPieces(count) {
+    for (let i = 0; i < rocketPieces.length; i++) {
+      if (i === count - 1) {
+        rocketPieces[i].style.visibility = 'visible';
+      } 
+     }
+       //Checking if incorrect count is equal to rocketPieces to display loss message
+    if (incorrectCount === rocketPieces.length  ) {
+        lMessage.style.visibility = 'visible';
+        resetButton.style.backgroundColor = 'red';
+    }
+   
+}
+  
 
 //Get letter elements from button
 let libraryLetters = document.querySelectorAll(".letter");
@@ -54,7 +86,9 @@ function compareLetters(sLetters, hWord) {
  
     if (hWord.includes(sLetters[i])){
 
-        // Different output for each matched letter
+        // console.log(hWord)
+
+        // Different output for each correct matched letter
         if (sLetters[i] === "g") {
             gStyle.style.visibility = 'visible';
             gButton.setAttribute("disabled", "");
@@ -76,41 +110,82 @@ function compareLetters(sLetters, hWord) {
         } else if (sLetters[i] === "y" ) {
             yStyle.style.visibility = 'visible';
             yButton.setAttribute("disabled", "");
-        }
+        } 
+        // Incorrect answers 
     } else if (sLetters[i] === "b" ) {
         bButton.setAttribute("disabled", "")
         bButton.style.backgroundColor = 'grey';
+        rocketPieces[incorrectCount].setAttribute("disabled", "");
+        displayRocketPieces(incorrectCount + 1);
+        incorrectCount++;
     } else if (sLetters[i] === "e" ) {
         eButton.setAttribute("disabled", "")
         eButton.style.backgroundColor = 'grey';
+        rocketPieces[incorrectCount].setAttribute("disabled", "");
+        displayRocketPieces(incorrectCount + 1);
+        incorrectCount++;
     } else if (sLetters[i] === "f" ) {
         fButton.setAttribute("disabled", "")
         fButton.style.backgroundColor = 'grey';
+        rocketPieces[incorrectCount].setAttribute("disabled", "");
+        displayRocketPieces(incorrectCount + 1);
+        incorrectCount++;
     } else if (sLetters[i] === "h" ) {
         hButton.setAttribute("disabled", "")
         hButton.style.backgroundColor = 'grey';
+        rocketPieces[incorrectCount].setAttribute("disabled", "");
+        displayRocketPieces(incorrectCount + 1);
+        incorrectCount++;
     } else if (sLetters[i] === "k" ) {
         kButton.setAttribute("disabled", "")
         kButton.style.backgroundColor = 'grey';
+        rocketPieces[incorrectCount].setAttribute("disabled", "");
+        displayRocketPieces(incorrectCount + 1);
+        incorrectCount++;
     } 
 
    }
 
    
-    //let selectedLetters = selectedLetters[i];
+//let selectedLetters = selectedLetters[i];
 console.log(hWord, sLetters)
 
 }
 
+//CREATING A RESET BUTTON-------------------
+resetButton.addEventListener('click', handleReset);
+
+//Reset rocket pieces
+function handleReset() {
+    for (let i = 0; i < rocketPieces.length; i++) {
+        rocketPieces[i].style.visibility = 'hidden';
+
+}
+
+//Reset buttons
+const buttons = document.querySelectorAll('.letter');
+  buttons.forEach((button) => {
+    button.removeAttribute('disabled');
+    button.style.backgroundColor = 'rgb(128, 170, 207)';
+  });
+
+  //Reset hidden word
+  const hiddenCorrectWord = document.querySelectorAll('#hiddenLetters');
+  hiddenCorrectWord.forEach((hiddenLetter) => {
+    hiddenLetter.style.visibility = 'hidden';
+  });
+//Reset selected letters array
+selectedLetters = [];
+
+//Reset incorrect count
+incorrectCount = 0;
+}
+// RESET BUTTON CREATED-------------------
+
 console.log(hiddenWord);
-//console.log("selected letters",selectedLetters);
+
+console.log(incorrectCount);
 
 
-//library of buttons with letter displayed.
 
 
-//Create Grid in CSS to hold library of 12 letters. By default all letters will be transparent. When the array matches, then change visibilty. 
-
-//Disable the button after clicked
-
-//
